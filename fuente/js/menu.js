0042f50e4688a933ac96409ac3c4e89784b4c2d5
Mia <doli.mia.fate.s@gmail.com>
2024-02-26 19:43:45 +0100

    Home Page v1.2

--- fuente/js/menu.js
+++ fuente/js/menu.js
@@ -1,7 +1,7 @@
 document.getElementById("menu").addEventListener("click", () => {
-    document.getElementById("menu_desplegable").classList.toggle("mostrar-menu");
+    document.getElementById("menu_desplegable").classList.toggle("aparecer");
 })
 
 document.getElementById("menu_desplegable").addEventListener("scroll", (evento) => {
-    evento.target.classList.remove("mostrar-menu");
+    evento.target.classList.remove("aparecer");
 })
