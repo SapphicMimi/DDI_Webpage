16db01feeb1ecdb5df394a51745ba26f2a87f55b
Mia <doli.mia.fate.s@gmail.com>
2024-02-29 21:29:33 +0100

    Manga v1.0

---
+++ fuente/js/inicio.js
@@ -0,0 +1,7 @@
+document.getElementById("logo").addEventListener("click", () => {
+    location.href = "../index.html";
+})
+
+document.getElementById("logoInicio").addEventListener("click", () => {
+    location.href = "index.html";
+})
