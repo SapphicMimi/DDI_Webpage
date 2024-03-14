b003b05b707a8a6fcc56aedbee7dc356735e3e4d
Mia <doli.mia.fate.s@gmail.com>
2024-03-14 01:31:26 +0100

    Nuevos Titulos e Imagenes de Moviles y Fuentes más grandes

--- fuente/js/inicio.js
+++ fuente/js/inicio.js
@@ -1,7 +1,3 @@
 document.getElementById("logo").addEventListener("click", () => {
     location.href = "../index.html";
 })
-
-document.getElementById("logoInicio").addEventListener("click", () => {
-    location.href = "index.html";
-})
